35b37199060354ad1c7a3a53784af5373d389260
Stefan Heller <sthefan.heller6341@gmail.com>
2022-02-17 19:22:34 -0500

    restructuring project

--- Handlers/main.go
+++ Handlers/main.go
@@ -1,50 +1,90 @@
 package handlers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"strings"
-	"fmt"
 )
 
-//TODO: move all Endpoints structs and such to this module
+var Mux *http.ServeMux
 
-var Requests = []string{"GET", "POST", "PUT", "DELETE", "HEAD", "CONNECT", "OPTIONS", "TRACE", "PATCH"}
+type Endpoint struct {
+	name   string
+	method string
+	action string
+}
+
+type Endpoints struct {
+	endpoints []*Endpoint
+}
+
+func (e *Endpoints) generateEndpoint(name, action, req string) {
+	if IsRequestType(name) {
+		e.Bind(&Endpoint{
+			name:   strings.ToUpper(name),
+			action: action,
+		})
+	}
+}
+
+func (e *Endpoints) Bind(ep *Endpoint) {
+	e.endpoints = append(e.endpoints, ep)
+}
+
+func (e *Endpoints) BindListeners() {
+	for _, end := range e.endpoints {
+		Mux.Handle(end.name, end)
+	}
+}
+
+func (e *Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == e.method {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`{ "message": %s }`, e.action)))
+	}
+}
+
+func InitMux() {
+	Mux = http.NewServeMux()
+}
+
+func NewEndpoint(name, action, method string) *Endpoint {
+	return &Endpoint{
+		name:   name,
+		action: action,
+		method: method,
+	}
+}
+
+func NewEndpoints() *Endpoints {
+	return &Endpoints{
+		endpoints: []*Endpoint{},
+	}
+}
+
+func Serve(port int) {
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), Mux))
+}
 
 func IsRequestType(name string) bool {
-	for _, v := range Requests {
-		if strings.ToUpper(name) == v {
-			return true
-		}
-	}
-	return false
-}
-
-func Get(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	path := r.URL.Path
-	w.Write([]byte(fmt.Sprintf(`{ "message": %s}`, path)))
-	
-}
-
-//func (e *Endpoints) GetRecord(path string){
-//	//TODO: implement this
-//}
-
-func Post(w http.ResponseWriter, r *http.Request) {
 	//TODO
+	return true
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	//TODO
+	//	TODO
 }
 
 func Put(w http.ResponseWriter, r *http.Request) {
 	//TODO
+
 }
 
 func Head(w http.ResponseWriter, r *http.Request) {
 	//TODO
+
 }
 
 func Connect(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -1,124 +1,18 @@
 package main
 
 import (
-	"GoPI/handlers"
+	h "GoPI/Handlers"
 	"fmt"
-	"log"
-	"net/http"
-	"strings"
 )
-
-type RequestType int64
-
-var mux *http.ServeMux
-
-const (
-	GET RequestType = iota
-	POST
-	PUT
-	DELETE
-	HEAD
-	CONNECT
-	OPTIONS
-	TRACE
-	PATCH
-)
-
-type CRUDType int64
-
-const (
-	CREATE CRUDType = iota
-	READ
-	UPDATE
-	DEL
-)
-
-type Endpoint struct {
-	name        string
-	requestType RequestType
-	action      string
-}
-
-type Endpoints struct {
-	endpoints []*Endpoint
-}
-
-func (e *Endpoints) generateEndpoint(name, action string, req RequestType) {
-	if handlers.IsRequestType(name) {
-		e.bind(&Endpoint{
-			name:   strings.ToUpper(name),
-			action: action,
-		})
-	}
-}
-
-func (e *Endpoints) bind(ep *Endpoint) {
-	e.endpoints = append(e.endpoints, ep)
-}
-
-func (e *Endpoints) bindListeners() {
-	for _, end := range e.endpoints {
-		//switch end.requestType {
-		//case GET:
-		//	mux.Handle(end.name, end)
-		//case POST:
-		//	mux.HandleFunc(end.name, handlers.Post)
-		//case PUT:
-		//	mux.HandleFunc(end.name, handlers.Put)
-		//case DELETE:
-		//	mux.HandleFunc(end.name, handlers.Delete)
-		//case OPTIONS:
-		//	mux.HandleFunc(end.name, handlers.Options)
-		//case TRACE:
-		//	mux.HandleFunc(end.name, handlers.Trace)
-		//case PATCH:
-		//	mux.HandleFunc(end.name, handlers.Patch)
-		//}
-		mux.Handle(end.name, end)
-	}
-}
-
-func (e *Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{ "message": %s }`, e.action)))
-}
-
-//func index(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "application/json")
-//	switch r.Method {
-//	case "GET":
-//		w.WriteHeader(http.StatusOK)
-//		w.Write([]byte(fmt.Sprintf(`{ "message": %s}`, r.URL.Path)))
-//	case "POST":
-//		w.WriteHeader(http.StatusOK)
-//		w.Write([]byte(`{ "message": "POST request received"}`))
-//	case "PUT":
-//		w.WriteHeader(http.StatusOK)
-//		w.Write([]byte(`{ "message": "PUT request received"}`))
-//	case "DELETE":
-//		w.WriteHeader(http.StatusOK)
-//		w.Write([]byte(`{ "message": "DELETE request received"}`))
-//	default:
-//		w.WriteHeader(http.StatusNotFound)
-//		w.Write([]byte(`{ "message": "not found"}`))
-//
-//	}
-//}
 
 func main() {
-	mux = http.NewServeMux()
-	endpoints := Endpoints{}
-	e := &Endpoint{
-		name:        "/test",
-		action:      "this is a test",
-		requestType: GET,
-	}
-	endpoints.bind(e)
-	endpoints.bindListeners()
+	h.InitMux()
+	e := h.NewEndpoint("/test", "this is a test", "GET")
+	endpoints := h.NewEndpoints()
+	endpoints.Bind(e)
+	endpoints.BindListeners()
 
 	fmt.Printf("Server is running....\n")
-	//mux.Handle("/", e)
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	h.Serve(8080)
 }
